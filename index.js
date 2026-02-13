1d1271d14e053c1b0f2caa8c8029470489657078
Ben Schalom KAMGA <benschalom8@gmail.com>
2026-02-13 12:00:10 -0500

    Merge du main

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './src/routes/pockemon.route.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync('./src/config/documentation.json', 'utf8')
+);
+
+const swaggerOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "Demo API"
+};
 
 dotenv.config();
 
@@ -8,8 +19,9 @@ const app = express()
 const PORT = process.env.PORT;  
 const HOST = process.env.HOST;
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
-app.use('/pockemon',router)
+app.use('/pokemon',router)
 
 app.listen(PORT, HOST, () => {
     console.log(`Serveur démarré sur http://${HOST}:${PORT}`);
