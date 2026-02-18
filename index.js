6782d58b7b3f0206758b25b01d02e4a5e1b5591d
Ben Schalom KAMGA <benschalom8@gmail.com>
2026-02-18 06:57:52 -0500

    Documentation liste pagine

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const HOST = process.env.HOST;
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
-app.use('/api/pokemon',router)
+app.use('/api/pokemons',router)
 
 
 app.listen(PORT, HOST, () => {
